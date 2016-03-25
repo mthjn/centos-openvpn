10a00f3a8eb6a812129c3957b0eeb69f136c4053
I won't tell <mthjn@users.noreply.github.com>
2016-03-25 20:09:29 +0100

    Create step1.sh

---
+++ step1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Do it as root" 1>&2
+  exit 1
+fi
+
+echo "Preparatory tasks ... "
+
+yum install epel-release
+yum install openvpn easy-rsa -y
+cp /usr/share/doc/openvpn-*/sample/sample-config-files/server.conf /etc/openvpn
+
+echo 'Manually edit ”/etc/openvpn/server.conf” ====> uncomment:'
+echo 'push "redirect-gateway def1 bypass-dhcp"'
+echo 'push "dhcp-option DNS 8.26.56.26"'
+echo 'push "dhcp-option DNS 8.20.247.20"'
+echo 'user nobody; group nobody'
+
