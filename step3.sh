8c510535d957ff3dbfa8b1ff11c919090af4ec61
I won't tell <mthjn@users.noreply.github.com>
2016-03-27 13:44:25 +0200

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -47,10 +47,24 @@ systemctl start iptables
 iptables --flush
 
 #internal routing: on OpenVZ the interface is venet0 not eth0
-echo "Internal routing: this uses interface eth0. If you have venet0 (check by running ifconfig), edit it in /etc/sysconfig/iptables."
-pause 'Fine [Enter]'
+echo "Internal routing: Checking what device you got ..."
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+ETH=`grep "eth0" /proc/net/dev`
+VEN=`grep "venet0" /proc/net/dev`
+
+if  [ -n "$ETH" ] ; then
+	echo Found device eth0 ...
+	DEVICE="eth0"
+elif [ -n "$VEN" ] ; then
+	echo Found device venet0 ...
+	DEVICE="venet0"
+else
+	echo no device found
+fi
+
+pause 'Whatever that means [Enter]'
+
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $DEVICE -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
@@ -63,7 +77,13 @@ systemctl -f enable openvpn@server.service
 systemctl start openvpn@server.service
 
 echo "Grepping netstat to see if openvpn is there ... "
-netstat -taupen | grep "openvpn"
-pause 'Fine [Enter]'
+OVPN=`netstat -taupen | grep "openvpn"`
+if  [ -n "$OVPN" ] ; then
+  echo "OpenVPN active."
+else
+  echo "Something wicked. OpenVPN not found in ”netstat -taupen”. Check firewall and tuntap device."
+fi
 
-echo "Конец фильма."
+pause 'OK [Enter]'
+
+echo "~~~ Конец фильма ~~~"
